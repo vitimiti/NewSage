0038fc0ee7c7cc9737b00642db241bd64c63b7f7
Víctor Matía Rodríguez <vmatir@outlook.com>
2025-12-28 20:32:33 +0000

    Add vector and quaternion record types to NewSage.WwVegas for mathematical operations

---
+++ NewSage.WwVegas/IoVector4.cs
@@ -0,0 +1,23 @@
+// -----------------------------------------------------------------------
+// <copyright file="IoVector4.cs" company="NewSage">
+// A transliteration and update of the CnC Generals (Zero Hour) engine and games with mod-first support.
+// Copyright (C) 2025 NewSage Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace NewSage.WwVegas;
+
+public record IoVector4(float X, float Y, float Z, float W);
