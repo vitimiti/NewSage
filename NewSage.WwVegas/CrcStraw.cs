416c2f3c873e1fdb50b6c710ff6401bc12164803
Víctor Matía Rodríguez <vmatir@outlook.com>
2025-12-28 23:49:27 +0000

    Add `CrcStraw` class to NewSage.WwVegas for CRC-based data handling in streams

---
+++ NewSage.WwVegas/CrcStraw.cs
@@ -0,0 +1,40 @@
+// -----------------------------------------------------------------------
+// <copyright file="CrcStraw.cs" company="NewSage">
+// A transliteration and update of the CnC Generals (Zero Hour) engine and games with mod-first support.
+// Copyright (C) 2025 NewSage Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace NewSage.WwVegas;
+
+public class CrcStraw : Straw
+{
+    public long Result => Crc;
+
+    protected CrcEngine Crc { get; } = new();
+
+    public override int Get(Span<byte> buffer)
+    {
+        if (buffer.Length < 1)
+        {
+            return 0;
+        }
+
+        var counter = base.Get(buffer);
+        _ = Crc.ToInt64(buffer[..counter]);
+        return counter;
+    }
+}
