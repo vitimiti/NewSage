05c93efdb99d528f6251060680f41b235bbee9be
Víctor Matía Rodríguez <vmatir@outlook.com>
2025-12-31 14:39:01 +0000

    Fix typo in `SimplePersistFactory` filename header.

--- NewSage.WwVegas/WwSaveLoad/SimplePersistFactory`1.cs
+++ NewSage.WwVegas/WwSaveLoad/SimplePersistFactory`1.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="SimplePersistFactor`1.cs" company="NewSage">
+// <copyright file="SimplePersistFactory`1.cs" company="NewSage">
 // A transliteration and update of the CnC Generals (Zero Hour) engine and games with mod-first support.
 // Copyright (C) 2025 NewSage Contributors
 //
