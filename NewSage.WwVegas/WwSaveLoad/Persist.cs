673d6271b00d459aee22d45add9e640c56881082
Víctor Matía Rodríguez <vmatir@outlook.com>
2025-12-31 14:37:45 +0000

    Make `PostLoadable` abstract and add `Persist` class with `Save` and `Load` methods to `WwSaveLoad`.

--- NewSage.WwVegas/WwSaveLoad/Persist.cs
+++ NewSage.WwVegas/WwSaveLoad/Persist.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="PostLoadable.cs" company="NewSage">
+// <copyright file="Persist.cs" company="NewSage">
 // A transliteration and update of the CnC Generals (Zero Hour) engine and games with mod-first support.
 // Copyright (C) 2025 NewSage Contributors
 //
@@ -20,9 +20,11 @@
 
 namespace NewSage.WwVegas.WwSaveLoad;
 
-public abstract class PostLoadable
+public abstract class Persist : PostLoadable
 {
-    public bool IsPostLoadRegistered { get; set; }
+    public abstract PersistFactory Factory { get; }
 
-    public virtual void OnPostLoad() { }
+    public virtual bool Save(ChunkSave cSave) => true;
+
+    public virtual bool Load(ChunkLoad cLoad) => true;
 }
