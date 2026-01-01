fda2b4cefaafe911509f09a9d28074f22c8691f6
Víctor Matía Rodríguez <vmatir@outlook.com>
2026-01-01 14:42:41 +0000

    Add initial implementation of `SageGame` with profiling, configuration, and initialization support.

---
+++ NewSage.Game/GameJsonContext.cs
@@ -0,0 +1,37 @@
+// -----------------------------------------------------------------------
+// <copyright file="GameJsonContext.cs" company="NewSage">
+// A transliteration and update of the CnC Generals (Zero Hour) engine and games with mod-first support.
+// Copyright (C) 2025 NewSage Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace NewSage.Game;
+
+[SuppressMessage(
+    "StyleCop.CSharp.ReadabilityRules",
+    "SA1106:Code should not contain empty statements",
+    Justification = "Generated."
+)]
+[JsonSourceGenerationOptions(
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower,
+    GenerationMode = JsonSourceGenerationMode.Default
+)]
+[JsonSerializable(typeof(GameOptions))]
+internal partial class GameJsonContext : JsonSerializerContext;
---
+++ NewSage.Game/SageGame.cs
@@ -0,0 +1,89 @@
+// -----------------------------------------------------------------------
+// <copyright file="SageGame.cs" company="NewSage">
+// A transliteration and update of the CnC Generals (Zero Hour) engine and games with mod-first support.
+// Copyright (C) 2025 NewSage Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using NewSage.Debug;
+using NewSage.Profile;
+
+namespace NewSage.Game;
+
+internal sealed class SageGame : IDisposable
+{
+    private readonly GameOptions _options;
+
+    private Profiler? _profiler;
+    private bool _disposed;
+
+    public SageGame(string configPath = "settings.json")
+    {
+        _options = LoadOptions(configPath);
+    }
+
+    public void Run() => Initialize();
+
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        _profiler?.Dispose();
+
+        _disposed = true;
+    }
+
+    [SuppressMessage(
+        "Design",
+        "CA1031:Do not catch general exception types",
+        Justification = "Do not throw during settings loading, we have defaults."
+    )]
+    private static GameOptions LoadOptions(string path)
+    {
+        if (!File.Exists(path))
+        {
+            return new GameOptions();
+        }
+
+        try
+        {
+            var json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize(json, GameJsonContext.Default.GameOptions) ?? new GameOptions();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Failed to load settings from file '{path}'.\n{ex}");
+            return new GameOptions();
+        }
+    }
+
+    private void Initialize()
+    {
+        if (_options.EnableProfiling)
+        {
+            _profiler = Profiler.Start($"{nameof(SageGame)}.{nameof(Initialize)}");
+        }
+
+        UnhandledExceptionHandler.Install(_options.DumpOptions);
+
+        _profiler?.Dispose();
+    }
+}
